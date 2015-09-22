7e2c2ae2e3eeee1c3ffc3270e87c1794890eaa5d
Sander <masterfenrir@outlook.com>
2015-09-22 22:43:36 +0200

    Bluetooth works, but still needs refactoring.

--- app/src/main/java/com/app/sander/bluetoothtest/MainActivity.java
+++ app/src/main/java/com/app/sander/bluetoothtest/MainActivity.java
@@ -3,32 +3,31 @@ package com.app.sander.bluetoothtest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.ParcelUuid;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
+/**
+ * Main activity of this application
+ */
 public class MainActivity extends AppCompatActivity {
 
     protected BluetoothAdapter mBluetoothAdapter;
     private ListView itemList;
-    private ArrayList<String> listData;
-    private ArrayAdapter<String> listAdapter;
+    private ArrayList<BluetoothDevice> listData;
+    private BluetoothListAdapter listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +35,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         itemList = (ListView) findViewById(R.id.listView);
         listData = new ArrayList<>();
-        listAdapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1,
-                listData);
+        listAdapter = new BluetoothListAdapter(this, 0, listData);
         itemList.setAdapter(listAdapter);
+        itemList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View viewClicked, int position, long id) {
+                TextView text = (TextView) viewClicked.findViewById(R.id.device_mac);
+                connectToDevice(text.getText().toString());
+            }
+        });
     }
 
     @Override
@@ -64,8 +68,13 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void connectBlueTooth(View view) {
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    /**
+     * Show the paired bluetooth devices
+     *
+     * @param view
+     */
+    public void showBluetoothDevices(View view) {
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
         listData.clear();
         listAdapter.notifyDataSetChanged();
@@ -73,20 +82,34 @@ public class MainActivity extends AppCompatActivity {
             // Loop through paired devices
             for (BluetoothDevice device : pairedDevices) {
                 // Add the name and address to an array adapter to show in a ListView
-                listData.add("\n" + device.getName() + "\n" + device.getAddress());
+                listData.add(device);
                 listAdapter.notifyDataSetChanged();
             }
         }
     }
 
-    private class ConnectThread extends Thread {
+    /**
+     * Connect to the chosen device
+     *
+     * @param address
+     */
+    private void connectToDevice(String address) {
+        Log.d("Measurement", address);
+        BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
+        new Thread(new BluetoothConnection(device)).start();
+    }
+
+    /**
+     * Inner class to handle an outgoing Bluetooth connection
+     */
+    private class BluetoothConnection implements Runnable {
 
         private static final String UUID_STRING = "34824060-611f-11e5-a837-0800200c9a66";
 
         private final BluetoothSocket mmSocket;
         private final BluetoothDevice mmDevice;
 
-        public ConnectThread(BluetoothDevice device) {
+        public BluetoothConnection(BluetoothDevice device) {
             // Use a temporary object that is later assigned to mmSocket,
             // because mmSocket is final
             BluetoothSocket tmp = null;
@@ -101,6 +124,7 @@ public class MainActivity extends AppCompatActivity {
             mmSocket = tmp;
         }
 
+        @Override
         public void run() {
             // Cancel discovery because it will slow down the connection
             mBluetoothAdapter.cancelDiscovery();
@@ -109,13 +133,17 @@ public class MainActivity extends AppCompatActivity {
                 // Connect the device through the socket. This will block
                 // until it succeeds or throws an exception
                 mmSocket.connect();
+                giveStatusUpdate(getString(R.string.connection_success));
+                byte[] array = new byte["Hello from the other device!".getBytes().length];
+                mmSocket.getInputStream().read(array);
+                giveStatusUpdate(new String(array, "UTF-8"));
             } catch (IOException connectException) {
                 // Unable to connect; close the socket and get out
+                giveStatusUpdate(getString(R.string.connection_error));
                 try {
                     mmSocket.close();
                 } catch (IOException closeException) {
                 }
-                return;
             }
 
             // Do work to manage the connection (in a separate thread)
@@ -130,6 +158,19 @@ public class MainActivity extends AppCompatActivity {
                 mmSocket.close();
             } catch (IOException e) {
             }
+        }
+
+        /**
+         * Give a status update using Toast.
+         *
+         * @param text The text to display
+         */
+        private void giveStatusUpdate(final String text) {
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
+                }
+            });
         }
     }
 
